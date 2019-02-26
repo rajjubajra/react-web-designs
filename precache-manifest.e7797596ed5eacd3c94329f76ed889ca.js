d6653e68598e75613a38f6381ca65558a7ce1f93
rajju <vajrachara@yahoo.com>
2019-02-26 23:36:55 +0000

    Updates

---
+++ precache-manifest.e7797596ed5eacd3c94329f76ed889ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web-designs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "edadf86e8793ff6539a7",
+    "url": "/react-web-designs/static/js/runtime~main.edadf86e.js"
+  },
+  {
+    "revision": "dbc8a79c5931b174908e",
+    "url": "/react-web-designs/static/js/main.dbc8a79c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-web-designs/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "dbc8a79c5931b174908e",
+    "url": "/react-web-designs/static/css/main.d05b1a76.chunk.css"
+  },
+  {
+    "revision": "aa888629640e0a128c784370b502dda9",
+    "url": "/react-web-designs/index.html"
+  }
+];
